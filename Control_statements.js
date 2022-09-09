9489aedfe7c0b5a4c78145b65e1eb90374956e39
Zenah Tegulwa <zainab.tegulwa783@gmail.com>
2022-09-09 13:10:03 +0300

    If Statements

---
+++ Control_statements.js
@@ -0,0 +1,16 @@
+let weight = 10;
+// IF Statement
+if(weight < 50){
+    console .log("You are unweight to donate blood")
+}
+
+let salary = 700000;
+let PAYEE = 30;
+
+if(salary < 300000){
+    console .log("You are not Taxable")
+}
+
+if(salary > 300000){
+    console .log(0.3*700000)
+}
