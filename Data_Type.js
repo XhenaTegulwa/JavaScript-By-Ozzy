16bbff3b7f89a43005ba0ed732ed0e16aed16c2e
Zenah Tegulwa <zainab.tegulwa783@gmail.com>
2022-09-09 12:36:55 +0300

    Added Data Types

---
+++ Data_Type.js
@@ -0,0 +1,17 @@
+// Data types define the kind of data to be stored in the computer memory
+//Examples of Data Types
+// 1. Intergers are numbers
+// 2. String is a collection of characters eg Xhena
+// 3 .Big Intergers are numbers whose length is 53 and more and one cant add big int to intergers
+// 4. Boolean there are data types that take on True/false or Yes/No
+// 5. Object (Objects data types have many mare semi data types eg null, arrays etc)
+var fName = "Xhena"; //String
+var myAge = 17; //Integer
+const datachecked = true; //Boolean
+var notNull = null;
+
+console .log (typeof(fName));
+console .log (typeof(myAge));
+console .log (typeof(datachecked));
+console .log (typeof(notNull));
+
